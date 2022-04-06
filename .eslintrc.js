bab1b9f4bd4eb610773ed3399d48188137067fb0
encorey <kosz.kosz@protonmail.com>
2022-04-06 17:17:07 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+    root: true,
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        project: './tsconfig.json',
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    settings: {
+        react: {
+          createClass: 'createReactClass',
+          pragma: 'React',
+          version: 'detect',
+        },
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+    },
+    "rules": {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+      'no-unused-vars': 'off',
+      'no-restricted-imports': 'off',
+      'no-use-before-define': 'off',
+      'import/prefer-default-export': 'off',
+      'max-len': 'off',
+      'func-names': 0,
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'default-param-last' : 'off',
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          "js": "never",
+          "jsx": "never",
+          "ts": "never",
+          "tsx": "never"
+        }
+     ]
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
